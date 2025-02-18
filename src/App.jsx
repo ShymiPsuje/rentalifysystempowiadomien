fddec00c72c9034fb4e4d3e5e0fa7938987d4403
ShymiPsuje <filmujemyyt@gmail.com>
2025-02-18 15:28:28 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NotificationExplorer from './components/NotificationExplorer';
+import './styles/globals.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <NotificationExplorer />
+    </div>
+  );
+}
+
+export default App;
